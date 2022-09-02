ce6ea5650861c911b1c4cf5fd87c187dee9b379f
Nicolas Toussaint <ntoussai29@gmail.com>
2022-09-03 01:14:31 +0200

    Load Nexity Bien dataclass

--- immo_scrap/nexity.py
+++ immo_scrap/nexity.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from typing import List
+from enum import Enum
+from typing import Any, Dict, List, Union
 import pandas as pd
 from bs4 import BeautifulSoup, ResultSet, Tag
 
@@ -14,7 +15,7 @@ def extract_table_header(table: Tag) -> Tag:
 
 def extract_title_from_table(table: Tag) -> str:
     header = extract_table_header(table)
-    return header.select("strong")[0].text
+    return header.select("strong")[0].text.strip()
 
 
 def extract_n_theoric_from_table(table: Tag) -> int:
@@ -27,7 +28,7 @@ def extract_n_theoric_from_table(table: Tag) -> int:
 
 def extract_columns_name_from_table(table: Tag) -> List[str]:
     column_name_cells = table.select(".content--details--header .cell")
-    columns_name = [cell.text for cell in column_name_cells]
+    columns_name = [cell.text.strip() for cell in column_name_cells]
     columns_name = columns_name[:-1]  # Remove Plan 2D / 3D (download item)
     return columns_name
 
@@ -38,7 +39,7 @@ def extract_elements_from_table(table: Tag) -> ResultSet:
 
 def extract_cells_text_from_element(element: Tag) -> List[str]:
     cells = element.select(".cell")
-    cells_text = [cell.text for cell in cells]
+    cells_text = [cell.text.strip() for cell in cells]
     cells_text_without_download = cells_text[:-1]  # Remove Plan 2D / 3D (download item)
     return cells_text_without_download
 
@@ -59,19 +60,134 @@ def extract_dataframe_from_table(table: Tag) -> pd.DataFrame:
     return df
 
 
+class BienType(Enum):
+    APPARTEMENT = "Appartement"
+
+
+class Orientation(Enum):
+    NORTH_WEST = "Nord-Ouest"
+    SOUTH_EAST = "Sud-Est"
+    SOUTH_WEST = "Sud-Ouest"
+
+
+def convert_price_str_to_float(price: str) -> float:
+    price_amount_str = price.replace(" ", "").replace("€", "")
+    price_float = float(price_amount_str)
+    return price_float
+
+
+def convert_m2_str_to_int(size: str) -> int:
+    size_amount_str = size.replace(" ", "").replace("m²", "")
+    size_float = int(size_amount_str)
+    return size_float
+
+
+def convert_floor_str_to_int(etage: str) -> int:
+    etage_amount_str = etage[6:]
+    etage_float = int(etage_amount_str)
+    return etage_float
+
+
+def convert_n_inclus_str_to_int(n_inclus_str: str) -> int:
+    if n_inclus_str == "Non":
+        return 0
+    else:  # Format '$$ inclus'
+        return int(n_inclus_str[:-7])
+
+
+@dataclass
+class NexityBien:
+    type: BienType
+    price: float
+    price_low_tva: Union[None, float]
+    date_livraison: str
+    size: int
+    floor: int
+    orientation: Orientation
+    has_balcony: bool
+    has_terasse: bool
+    n_parking: int
+
+    @staticmethod
+    def extract_type(datas: Dict[str, Any]) -> BienType:
+        return BienType(datas["Type"])
+
+    @staticmethod
+    def extract_price(datas: Dict[str, Any]) -> float:
+        return convert_price_str_to_float(datas["Prix TVA 20%"])
+
+    @staticmethod
+    def extract_price_low_tva(datas: Dict[str, Any]) -> Union[None, float]:
+        key = "TVA réduite  (2)"
+        return convert_price_str_to_float(datas[key]) if key in datas else None
+
+    @staticmethod
+    def extract_size(datas: Dict[str, Any]) -> int:
+        return convert_m2_str_to_int(datas["Surface"])
+
+    @staticmethod
+    def extract_floor(datas: Dict[str, Any]) -> int:
+        return convert_floor_str_to_int(datas["Étage"])
+
+    @staticmethod
+    def extract_orientation(datas: Dict[str, Any]) -> Orientation:
+        return Orientation(datas["Orientation"])
+
+    @staticmethod
+    def extract_has_balcony(datas: Dict[str, Any]) -> bool:
+        return "Balcon" in datas["Les +"]
+
+    @staticmethod
+    def extract_has_terasse(datas: Dict[str, Any]) -> bool:
+        return "Terasse" in datas["Les +"]
+
+    @staticmethod
+    def extract_date_livraison(datas: Dict[str, Any]) -> str:
+        return datas["Livraison"]
+
+    @staticmethod
+    def extract_n_parking(datas: Dict[str, Any]) -> int:
+        key = "Parking"
+        return convert_n_inclus_str_to_int(datas[key]) if key in datas else 0
+
+    @classmethod
+    def create_from_dict(cls, datas: Dict[str, Any]):
+        return NexityBien(
+            type=cls.extract_type(datas),
+            price=cls.extract_price(datas),
+            price_low_tva=cls.extract_price_low_tva(datas),
+            date_livraison=cls.extract_date_livraison(datas),
+            size=cls.extract_size(datas),
+            floor=cls.extract_floor(datas),
+            orientation=cls.extract_orientation(datas),
+            has_balcony=cls.extract_has_balcony(datas),
+            has_terasse=cls.extract_has_terasse(datas),
+            n_parking=cls.extract_n_parking(datas),
+        )
+
+
+def extract_biens_from_table(table: Tag) -> List[NexityBien]:
+    columns = extract_columns_name_from_table(table)
+    values = extract_elements_values_from_table(table)
+    res = [
+        NexityBien.create_from_dict(dict(zip(columns, values_i))) for values_i in values
+    ]
+    return res
+
+
 @dataclass
 class NexityTable:
-    df: pd.DataFrame
+    biens: List[NexityBien]
     title: str
     n_theoric: int
 
     @classmethod
     def create_from_table_tag(cls, table: Tag) -> "NexityTable":
-        df = extract_dataframe_from_table(table)
+        biens = extract_biens_from_table(table)
         title = extract_title_from_table(table)
         n_theoric = extract_n_theoric_from_table(table)
         return NexityTable(
-            df=df,
+            biens=biens,
             title=title,
             n_theoric=n_theoric,
         )
--- tests/test_nexity.py
+++ tests/test_nexity.py
@@ -14,6 +14,11 @@ def nexity_table_tag(nexity_list_soup) -> Tag:
     return nexity.extract_tables_from_soup(nexity_list_soup)[0]
 
 
+@pytest.fixture
+def nexity_table_v2_tag(nexity_list_soup) -> Tag:
+    return nexity.extract_tables_from_soup(nexity_list_soup)[1]
+
+
 def test_extract_table_from_soup(nexity_list_soup):
     res = nexity.extract_tables_from_soup(nexity_list_soup)
     assert len(res) == 3
@@ -39,6 +44,21 @@ def test_extract_columns_name_from_table(nexity_table_tag):
         "Surface",
         "Étage",
         "Orientation",
+        "Les +",
+    ]
+
+
+def test_extract_columns_name_from_table_v2(nexity_table_v2_tag):
+    res = nexity.extract_columns_name_from_table(nexity_table_v2_tag)
+    assert res == [
+        "Type",
+        "TVA réduite  (2)",
+        "Prix TVA 20%",
+        "Livraison",
+        "Surface",
+        "Étage",
+        "Orientation",
+        "Parking",
         "Les +",
     ]
 
@@ -97,13 +117,127 @@ def test_extract_dataframe_from_table(nexity_table_tag):
     pd.testing.assert_series_equal(row_at_1, expected_row_at_1)
 
 
+class Test_NexityBien:
+    @pytest.mark.parametrize(
+        "datas, expected",
+        [
+            (
+                {
+                    "Type": "Appartement",
+                    "Prix TVA 20%": "303 700 €",
+                    "Livraison": "3ème trimestre 2025",
+                    "Surface": "46 m²",
+                    "Étage": "Étage 17",
+                    "Orientation": "Nord-Ouest",
+                    "Les +": "",
+                },
+                nexity.NexityBien(
+                    type=nexity.BienType.APPARTEMENT,
+                    price=303_700.00,
+                    price_low_tva=None,
+                    date_livraison="3ème trimestre 2025",
+                    size=46,
+                    floor=17,
+                    orientation=nexity.Orientation.NORTH_WEST,
+                    has_balcony=False,
+                    has_terasse=False,
+                    n_parking=0,
+                ),
+            ),
+            ## Table V2
+            (
+                {
+                    "Type": "Appartement",
+                    "TVA réduite  (2)": "493 000 €",
+                    "Prix TVA 20%": "560 760 €",
+                    "Livraison": "3ème trimestre 2025",
+                    "Surface": "88 m²",
+                    "Étage": "Étage 1",
+                    "Orientation": "Sud-Ouest",
+                    "Parking": "1 inclus",
+                    "Les +": "Balcon",
+                },
+                nexity.NexityBien(
+                    type=nexity.BienType.APPARTEMENT,
+                    price_low_tva=493_000.00,
+                    price=560_760.00,
+                    date_livraison="3ème trimestre 2025",
+                    size=88,
+                    floor=1,
+                    orientation=nexity.Orientation.SOUTH_WEST,
+                    has_balcony=True,
+                    has_terasse=False,
+                    n_parking=1,
+                ),
+            ),
+            ## Table V2 avec terasse
+            (
+                {
+                    "Type": "Appartement",
+                    "TVA réduite  (2)": "240 000 €",
+                    "Prix TVA 20%": "534 600 €",
+                    "Livraison": "3ème trimestre 2025",
+                    "Surface": "53 m²",
+                    "Étage": "Étage 13",
+                    "Orientation": "Sud-Ouest",
+                    "Parking": "4 inclus",
+                    "Les +": "Terasse",
+                },
+                nexity.NexityBien(
+                    type=nexity.BienType.APPARTEMENT,
+                    price_low_tva=240_000.00,
+                    price=534_600.00,
+                    date_livraison="3ème trimestre 2025",
+                    size=53,
+                    floor=13,
+                    orientation=nexity.Orientation.SOUTH_WEST,
+                    has_balcony=False,
+                    has_terasse=True,
+                    n_parking=4,
+                ),
+            ),
+        ],
+    )
+    def test_create_from_dict(self, datas, expected):
+        res = nexity.NexityBien.create_from_dict(datas)
+        assert res == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ("1 inclus", 1),
+        ("Non", 0),
+        ("13 inclus", 13),
+    ],
+)
+def test_convert_n_inclus_str_to_int(input, expected):
+    res = nexity.convert_n_inclus_str_to_int(input)
+    assert res == expected
+
+
+def test_extract_biens_from_table(nexity_table_tag):
+    res = nexity.extract_biens_from_table(nexity_table_tag)
+    assert isinstance(res, list)
+    assert len(res) == 8
+    assert isinstance(res[2], nexity.NexityBien)
+
+
+def test_extract_biens_from_table_v2(nexity_table_v2_tag):
+    res = nexity.extract_biens_from_table(nexity_table_v2_tag)
+    assert isinstance(res, list)
+    assert len(res) == 16
+    assert isinstance(res[10], nexity.NexityBien)
+
+
 class Test_NexityTable:
     def test_create_from_table_tag(self, nexity_table_tag):
         res = nexity.NexityTable.create_from_table_tag(nexity_table_tag)
         assert isinstance(res, nexity.NexityTable)
-        df = res.df
-        assert isinstance(df, pd.DataFrame)
-        assert df.shape == (8, 7)
+        biens = res.biens
+        assert isinstance(biens, list)
+        assert len(biens) == 8
+        assert isinstance(biens[1], nexity.NexityBien)
         assert res.title == "2 pièces"
         assert res.n_theoric == 8
 
